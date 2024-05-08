9e661277cd6b208a6c7d4cf51ed9380472531e8c
Abishek <avineshmtp@gmail.com>
2024-05-08 19:57:40 +0530

    commit

--- server/src/server.js
+++ server/src/server.js
@@ -2,7 +2,7 @@ const connect = require('./configs/db');
 const express = require('express');
 
 require("dotenv").config();
-const PORT = process.env.PORT || 6000;
+const PORT = process.env.PORT || 7000;
 
 const app = express();
 app.listen(PORT, async () => {
